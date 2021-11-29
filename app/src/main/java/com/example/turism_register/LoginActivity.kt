28cb2c5949e8095175da4ac0c4a405a90c6d4184
Shomira <shomirarosales01@gmail.com>
2021-11-29 11:00:40 -0500

    avance login y register

---
+++ app/src/main/java/com/example/turism_register/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.turism_register
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        tw_registrarse.setOnClickListener(){
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/example/turism_register/RegisterActivity.kt
+++ app/src/main/java/com/example/turism_register/RegisterActivity.kt
@@ -1,12 +1,11 @@
 package com.example.turism_register
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, LoginActivity::class.java))
+        setContentView(R.layout.activity_register)
     }
 }
