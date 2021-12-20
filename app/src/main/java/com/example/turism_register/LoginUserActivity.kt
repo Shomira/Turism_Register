2566b07247d76b258acdb7e3c8c27dd7e391cce4
Shomira <shomirarosales01@gmail.com>
2021-12-19 22:11:46 -0500

    Registro de usuarios y conexión a firebase

--- app/src/main/java/com/example/turism_register/LoginUserActivity.kt
+++ app/src/main/java/com/example/turism_register/LoginUserActivity.kt
@@ -3,8 +3,12 @@ package com.example.turism_register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login_user.*
-
+enum class ProviderType{
+    BASIC
+}
 class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +18,39 @@ class LoginUserActivity : AppCompatActivity() {
         tw_registrarse.setOnClickListener(){
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+        setup()
+    }
+    private fun setup(){
+        title = "Autentication"
 
-        btn_login.setOnClickListener() {
-            startActivity(Intent(this, MainActivity::class.java))
+        btn_login.setOnClickListener(){
+            if (ip_usuario.text.isNotEmpty() && ip_contrasenia.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(ip_usuario.text.toString(),
+                    ip_contrasenia.text.toString()).addOnCompleteListener(){
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email?:"", ProviderType.BASIC)
+                    }else{
+                        showAlert2()
+                    }
+                }
+            }
         }
+    }
+    private fun showAlert2(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autenticado ")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =builder.create()
+        dialog.show()
+
+
+    }
+    private fun showHome(email:String, provider: ProviderType){
+        val homeIntent:Intent=Intent(this,MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
--- app/src/main/java/com/example/turism_register/RegisterActivity.kt
+++ app/src/main/java/com/example/turism_register/RegisterActivity.kt
@@ -1,12 +1,101 @@
 package com.example.turism_register
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+
+import android.util.Patterns
+import com.example.turism_register.fragments.HomeFragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login_user.ip_contrasenia
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Pattern
+
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         supportActionBar?.hide()
+        val bundle: Bundle? = intent.extras
+        val provider: String? = bundle?.getString("provider")
+        auth = Firebase.auth
+
+        btn_registrar.setOnClickListener {
+
+            val mEmail = ip_correo_r.text.toString()
+            val mPassword = ip_contrasenia.text.toString()
+            val mRepeatPassword = ip_contrasenia2_r.text.toString()
+
+            val passwordRegex = Pattern.compile("^" +
+                    "(?=.*[-@#$%^&+=])" +     // Al menos 1 carácter especial
+                    ".{6,}" +                // Al menos 4 caracteres
+                    "$")
+
+            if(mEmail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+                Toast.makeText(this, "Ingrese un email valido.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mPassword.isEmpty() || !passwordRegex.matcher(mPassword).matches()){
+                Toast.makeText(this, "La contraseña es debil.",
+                    Toast.LENGTH_SHORT).show()
+            } else if (mPassword != mRepeatPassword){
+                Toast.makeText(this, "Confirma la contraseña.",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                db.collection("users").document(mEmail).set(
+                    hashMapOf(
+                        "provider" to provider, "address" to ip_correo_r.text.toString(),
+                        "usuario" to ip_usuario_r.text.toString(),
+                        "nombres" to ip_nombre_r.text.toString()
+                    )
+                )
+                createAccount(mEmail, mPassword)
+            }
+
+        }
+
+
+    }
+
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, HomeFragment::class.java)
+                    this.startActivity(intent)
+                } else {
+                    Toast.makeText(this, "No se pudo crear la cuenta. Vuelva a intertarlo",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    private fun showHome(email:String, provider: ProviderType){
+        val homeIntent: Intent = Intent(this,MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de registro ")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =builder.create()
+        dialog.show()
+
+
     }
 }
